25d65449e0d0fceea8f5258948072b0922210b4a
Younes El Bouhi <yel-bouh@e1r10p11.1337.ma>
2025-07-16 21:16:38 +0100

    C00

--- C00/ex04/ft_is_negative.c
+++ C00/ex04/ft_is_negative.c
@@ -2,13 +2,12 @@
 
 
 void ft_is_negative(int n){
-	if(n >= 0){
+	if(n <= 0)
 	write(1, "N", 1);
-		  }
-	else{
-	write(1, "P")
-	    }
+	else
+	write(1, "P", 1);
+	    
                           }
 int main(){
-	ft_is_negative();
+	ft_is_negative(-5);
 	  }
