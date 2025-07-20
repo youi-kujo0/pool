2b3deec10302754de93b64cc50359148da488d64
Younes El Bouhi <yel-bouh@e2r6p1.1337.ma>
2025-07-20 16:45:39 +0100

    c02

---
+++ C02/ex11/ft_putstr_non_printable.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+void ft_putstr_non_printable(char *str)
+{
+	int	i;
+	i = 0;
+	while(str[i] != 0)
+	{
+		if(str[i] < 32 || str[i] > 126)
+		{
+			char hex_dec[]= "0123456789abcdef";
+			char buff[2];
+
+			buff[0] = hex_dec[(str[i] >> 4) & 0x0f];
+			buff[1] = hex_dec[str[i] & 0x0f];
+			write(1, "\\", 1);
+			write(1, buff, 2);
+		}
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int main()
+{
+    char test_str[] = "Hello\nWorld\t!\x01\x7F";
+    ft_putstr_non_printable(test_str);
+    write(1, "\n", 1);
+    return 0;
+}
